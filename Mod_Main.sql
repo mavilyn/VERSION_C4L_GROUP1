377c201cd6513e811a94a963887c85ec0316203b
mavilyn <gladys.mavilyn@gmail.com>
2013-03-14 21:48:15 +0800

    remove old sql tables

--- Mod_Main.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE bill(
-	billeraccountnum NUMBER(10,0) NOT NULL,
-	refnum NUMBER(11,0) NOT NULL,
-	bill_cost NUMBER(10,0) NOT NULL,
-	CONSTRAINT bill_account_fk FOREIGN KEY(billeraccountnum)
-                REFERENCES ACCOUNT(accountnum),
-	CONSTRAINT bill_refnum_pk PRIMARY KEY(refnum)
-	/*,CONSTRAINT accountconnected_otheraccountnum_pk PRIMARY KEY(otheraccountnum)*/
-);

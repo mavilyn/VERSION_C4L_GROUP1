cda832e007d960b29e975b24b3285b2f820962c5
mavilyn <gladys.mavilyn@gmail.com>
2013-03-21 01:12:23 +0800

    with validations

--- GuestBankv1/removefrombillerlist.php
+++ GuestBankv1/removefrombillerlist.php
@@ -1,5 +1,23 @@
 <?php
 	$conn = oci_connect('mainbank', 'kayato1');
+	$query='delete from bills where biller_accontnum=:accountnum';
+	$parsedQuery = oci_parse($conn, $query);
+	oci_bind_by_name($parsedQuery, ':accountnum', $_REQUEST['accountnum']);
+	oci_execute($parsedQuery);
+	//oci_commit($conn);
+	
+	$query='delete from biller_cust where biller_accontnum=:accountnum';
+	$parsedQuery = oci_parse($conn, $query);
+	oci_bind_by_name($parsedQuery, ':accountnum', $_REQUEST['accountnum']);
+	oci_execute($parsedQuery);
+	//oci_commit($conn);
+	
+	$query='delete from current_biller where biller_accontnum=:accountnum';
+	$parsedQuery = oci_parse($conn, $query);
+	oci_bind_by_name($parsedQuery, ':accountnum', $_REQUEST['accountnum']);
+	oci_execute($parsedQuery);
+	oci_commit($conn);
+
 	$query='delete from billerlist where blist_accountnum=:accountnum';
 	$parsedQuery = oci_parse($conn, $query);
 	oci_bind_by_name($parsedQuery, ':accountnum', $_REQUEST['accountnum']);
